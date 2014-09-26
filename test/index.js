b97236fa35610747df2bd7912d1dd75c5ae1135e
Graham Ballantyne <grahamb@sfu.ca>
2014-09-26 15:55:18 -0700

    add tests

---
+++ test/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var should = require('should');
+var config = require('./config');
+var PhotoClient = require('../index');
+var assert = require('assert');
+
+describe('PhotoClient', function() {
+  describe('Initialization', function() {
+
+    it('should throw an error if no username passed', function(done) {
+      (function() { return new PhotoClient(); }).should.throw();
+      done();
+    });
+
+    it('should throw an error if no password passed', function(done) {
+      (function() { return new PhotoClient({username: 'test'}); }).should.throw();
+      done();
+    });
+
+    it('should not throw an error if both username and password passed', function(done) {
+      (function() { return new PhotoClient(config); }).should.not.throw();
+      done();
+    });
+
+  });
+});
