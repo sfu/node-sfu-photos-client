dae37c57f315d75be43a69e48207e66753809ee2
Graham Ballantyne <grahamb@sfu.ca>
2014-09-29 12:00:39 -0700

    replace should with chai

--- test/index.js
+++ test/index.js
@@ -1,29 +1,34 @@
 'use strict';
 
-var should = require('should');
+var chai = require('chai');
+var should = chai.should();
+var chaiAsPromised = require("chai-as-promised");
+chai.should();
+chai.use(chaiAsPromised);
+
 var config = require('./config');
 var PhotoClient = require('../index');
-var assert = require('assert');
+
 
 describe('Initialization', function() {
 
   it('should throw an error if no API endpoint passed', function(done) {
-    (function() { return new PhotoClient({endpoint: null}); }).should.throw();
+    (function() { return new PhotoClient({endpoint: null}); }).should.Throw();
     done();
   })
 
   it('should throw an error if no username passed', function(done) {
-    (function() { return new PhotoClient({username: null}); }).should.throw();
+    (function() { return new PhotoClient({username: null}); }).should.Throw();
     done();
   });
 
   it('should throw an error if no password passed', function(done) {
-    (function() { return new PhotoClient({username: 'test'}); }).should.throw();
+    (function() { return new PhotoClient({username: 'test'}); }).should.Throw();
     done();
   });
 
   it('should not throw an error if both username and password passed', function(done) {
-    (function() { return new PhotoClient(config); }).should.not.throw();
+    (function() { return new PhotoClient(config); }).should.not.Throw();
     done();
   });
 
